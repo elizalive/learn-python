4ccd2ac4a6cd3c3d522e3165a1c62b1510eb9ea8
Elizabeth Scheltens <elizabeth.scheltens@gmail.com>
2014-11-25 22:11:39 -0500

    Excercise 3: Numbers and Math

---
+++ ex3.py
@@ -0,0 +1,42 @@
+# Displays text "Now I will count my eggs"
+print "I will now count my chickens:"
+
+# Displays text "Hens,"
+# Calculates 30 / 6 (5) and adds 25 (30)
+print "Hens", 25 + 30 / 6
+
+# Displays text "Roosters,"
+# Calculates 25 * 3 (75)
+# Calculates the remainder of 75 / 4 (3)
+# Calculates 100 - 3 (97)
+print "Roosters", 100 - 25 * 3 % 4
+
+# Displays text "Now I will count my eggs"
+print "Now I will count the eggs:"
+
+# Calculates remainder of 4 / 2 (0)
+# Calculates 1 / 4 (0)
+# Calculates 3 + 2 + 1 - 5 + 0 - 0 + 6 (7)
+# Displays text "7"
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Displays text "Is it true that 3 + 2 < 5 - 7?" 
+print "Is it true that 3 + 2 < 5 - 7?"
+
+# Calculates 3 + 2 (5)
+# Calculates 5 - 7 (-2)
+# Asks if -2 is greater than 5
+# Displays text "False"
+print 3 + 2 < 5 - 7
+
+# Calculates 3 + 2 (5)
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
