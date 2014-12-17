9087a041a0729a072efea6483c02664c1e760940
Elizabeth Scheltens <elizabeth.scheltens@gmail.com>
2014-12-17 15:36:42 -0500

    ex7 and ex8
    More Printing and Printing, Printing

---
+++ ex7.py
@@ -0,0 +1,59 @@
+# Displays text 'Mary had a little lamb.'
+print "Mary had a little lamb."
+
+# Looks for string variable to replace %s
+# Replaces %s with 'snow'
+# Displays text 'Its fleece was white as snow.'
+print "Its fleece was white as %s." % 'snow'
+
+# Displays ten copies of the string '.'
+print "." * 10 #what'd that do?
+
+# Sets value of variable end1 equal to the string "C"
+end1 = "C"
+
+# Sets value of the variable end2 equal to the string "h"
+end2 = "h"
+
+# Sets value of the variable end3 equal to the string "e"
+end3 = "e"
+
+# Sets value of the variable end4 equal to the string "e"
+end4 = "e"
+
+# Sets value of the variable end5 equal to the string "s"
+end5 = "s"
+
+# Sets value of the variable end6 equal to the string "e"
+end6 = "e"
+
+# Sets value of the variable end7 equal to the string "B"
+end7 = "B"
+
+# Sets value of the variable end8 equal to the string "u"
+end8 = "u"
+
+# Sets value of the variable end9 equal to the string "r"
+end9 = "r"
+
+# Sets value of the variable end10 equal to the string "g"
+end10 = "g"
+
+# Sets value of the variable end11 equal to the string "e"
+end11 = "e"
+
+# Sets value of the variable end12 equal to the string "r"
+end12 = "r"
+
+# watch that comma at the end. try removing it to see what happens
+
+# Searches for values for variables end1 - end6
+# Replaces variables end1 - end6 with values above
+# Displays text 'Cheese'
+# Replaces , with a space
+print end1 + end2 + end3 + end4 + end5 + end6,
+
+# Searches for values for variables end7 - end12
+# Replaces variables end7 - end12 with values above
+# Displays text 'Burger'
+print end7 + end8 + end9 + end10 + end11 + end12
